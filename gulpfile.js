ed953f3ccf2e90e85ccd0f17c907f55c0a9b7440
Ralph van Barneveld <ralphje@gmail.com>
2018-03-04 20:32:50 +0100

    Add build tasks for fonts, images and css

--- gulpfile.js
+++ gulpfile.js
@@ -12,3 +12,28 @@ var imagemin = require('gulp-imagemin');
 var stats = require('gulp-stylestats');
 var htmllint = require('gulp-htmllint');
 var csslint = require('gulp-csslint');
+
+gulp.task("default", ['build']);
+
+// Build tasks
+gulp.task("make-css", function(){
+  return gulp.src('src/style/*.scss')
+          .pipe(sass())
+          .pipe(csso())
+          .pipe(gulp.dest('dist/css/'))
+});
+
+gulp.task('make-fonts', function () {
+    return gulp.src('src/fonts/*.ttf')
+        .pipe(fontmin({}))
+        .pipe(ttf2woff2())
+        .pipe(gulp.dest('dist/fonts'));
+});
+
+gulp.task('minify-images', () =>
+    gulp.src('src/img/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('dist/img'))
+);
+
+gulp.task("build", ['make-css','make-fonts', 'minify-images']);
