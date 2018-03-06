9dacdc09d9593f4dec8471dfd8ca156e4fb94acc
Ralph van Barneveld <ralphje@gmail.com>
2018-03-07 00:36:51 +0100

    webpack config and gulpfile update

--- gulpfile.js
+++ gulpfile.js
@@ -1,39 +1,19 @@
 var gulp = require('gulp');
-var watch = require('gulp-watch');
-// SCSS - CSS tools
-var sass = require('gulp-sass');
-var csso = require('gulp-csso');
-// Font tools
-var fontmin = require('gulp-fontmin');
-var ttf2woff2 = require('gulp-ttf2woff2');
-// Image tools
-var imagemin = require('gulp-imagemin');
-// Linters
-var stats = require('gulp-stylestats');
-var htmllint = require('gulp-htmllint');
-var csslint = require('gulp-csslint');
+// Logger
+require('gulp-stats')(gulp);
 
 gulp.task("default", ['build']);
 
-// Build tasks
-gulp.task("make-css", function(){
-  return gulp.src('src/style/*.scss')
-          .pipe(sass())
-          .pipe(csso())
-          .pipe(gulp.dest('dist/css/'))
-});
+gulp.task('make-css', require('./gulp-tasks/make-css')());
+gulp.task('minify-img', require('./gulp-tasks/minify-img')());
+gulp.task('make-fonts', require('./gulp-tasks/make-fonts')());
 
-gulp.task('make-fonts', function () {
-    return gulp.src('src/fonts/*.ttf')
-        .pipe(fontmin({}))
-        .pipe(ttf2woff2())
-        .pipe(gulp.dest('dist/fonts'));
-});
+gulp.task("build", ['make-css', 'minify-img', 'make-fonts']);
 
-gulp.task('minify-images', () =>
-    gulp.src('src/img/*')
-        .pipe(imagemin())
-        .pipe(gulp.dest('dist/img'))
-);
+gulp.task('lint-css', require('./gulp-tasks/lint-css')());
+gulp.task('lint-html', require('./gulp-tasks/lint-html')());
 
-gulp.task("build", ['make-css','make-fonts', 'minify-images']);
+
+gulp.task("lint", ['lint-css', 'lint-html']);
+
+//gulp.task("test", ['lint-css', 'lint-html']);
