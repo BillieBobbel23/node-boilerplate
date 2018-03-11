aaed766c3de60653bdc5574613d7f0dd9694b6b7
Ralph van Barneveld <ralphje@gmail.com>
2018-03-12 00:44:54 +0100

    allow for empty parameters in gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,26 @@
 var gulp = require('gulp');
 var path = require('path');
+// TODO: Allow calls without --i / --o parameters
+console.log(process.argv);
+
 var paths = {
-  in: path.resolve(__dirname, process.argv[4]),
-   out: path.resolve(__dirname, process.argv[6]),
+   in: process.argv[4] ? path.resolve(__dirname, process.argv[4]) : path.resolve(__dirname, 'src/'),
+   out: process.argv[6] ? path.resolve(__dirname, process.argv[6]): path.resolve(__dirname, 'dist/')
 };
 // Logger
 require('gulp-stats')(gulp);
 
-gulp.task("default", ['build']);
-
+// Build tasks
 gulp.task('make-css', require('./tools/make-css')(paths.in, paths.out));
 gulp.task('minify-img', require('./tools/minify-img')(paths.in, paths.out));
 gulp.task('make-fonts', require('./tools/make-fonts')(paths.in, paths.out));
-
-gulp.task("build", ['make-css', 'minify-img', 'make-fonts']);
-
+// Lint tasks
 gulp.task('lint-css', require('./tools/lint-css')(paths.out));
 gulp.task('lint-html', require('./tools/lint-html')(__dirname));
+// Watch
+gulp.task('watch', require('./tools/watch-assets')(__dirname));
 
+// Combined tasks
+gulp.task("default", ['build']);
+gulp.task("build", ['make-css', 'minify-img', 'make-fonts']);
 gulp.task("lint", ['lint-html', 'lint-css']);
