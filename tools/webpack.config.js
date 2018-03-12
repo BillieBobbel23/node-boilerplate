b793d8b39216f81ee03802f578da2d1df8d2c073
Ralph van Barneveld <ralphje@gmail.com>
2018-03-12 02:41:18 +0100

    Fixed ESlint task

--- tools/webpack.config.js
+++ tools/webpack.config.js
@@ -2,7 +2,10 @@ const path = require('path');
 
 module.exports = {
   mode: 'production',
-  entry: './src/js/empty.js',
+  entry: [
+    path.resolve(__dirname, '../src/js/empty.js'),
+    path.resolve(__dirname, '../src/js/not.js')
+  ],
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, '../dist/js/')
