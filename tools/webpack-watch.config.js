1c38845683afa6d8b96e347e105448b7fffddf14
Ralph van Barneveld <ralphje@gmail.com>
2018-03-12 02:48:41 +0100

    Split up Webpack config for watch and build

--- tools/webpack-watch.config.js
+++ tools/webpack-watch.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 
 module.exports = {
   mode: 'development',
+  watch: true,
   entry: [
     path.resolve(__dirname, '../src/js/empty.js'),
     path.resolve(__dirname, '../src/js/not.js')
