6f5560aa2f12a5acb639980aaca37c46d0e3dce1
Ralph van Barneveld <ralphje@gmail.com>
2018-05-01 22:47:59 +0200

    replace hardcoded values and extend config options for webpack

--- tools/config.js
+++ tools/config.js
@@ -1,5 +1,11 @@
 const CONFIG = {
   "DIRECTORY_INPUT": "../../src/",
-  "DIRECTORY_OUTPUT": "../../dist/"
+  "DIRECTORY_OUTPUT": "../../dist/",
+
+  "WEBPACK_BUNDLE_NAME": "bundle",
+  "WEBPACK_BUNDLED_FILES": [
+    "empty.js",
+    "not.js"
+  ]
 };
 module.exports = CONFIG;
--- tools/webpack.config.js
+++ tools/webpack.config.js
@@ -1,13 +1,14 @@
 const path = require('path');
+const CONFIG = require('./config.js');
 
 module.exports = {
   mode: 'production',
   entry: [
-    path.resolve(__dirname, '../src/js/empty.js'),
-    path.resolve(__dirname, '../src/js/not.js')
+    path.resolve(__dirname, CONFIG.WEBPACK_BUNDLED_FILES[0]),
+    path.resolve(__dirname, CONFIG.WEBPACK_BUNDLED_FILES[1])
   ],
   output: {
-    filename: 'bundle.js',
+    filename: `${CONFIG.WEBPACK_BUNDLE_NAME}-test.js`,
     path: path.resolve(__dirname, '../dist/js/')
   }
 };
