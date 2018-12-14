e63b883bfa7042f3a43b5c80db68b58b769928e9
Ralph van Barneveld <ralphje@gmail.com>
2018-12-14 13:42:45 +0100

    add CSS build to Webpack, also added CSSO

--- tools/webpack.dev.js
+++ tools/webpack.dev.js
@@ -1,24 +1,18 @@
 const path = require('path');
 const merge = require('webpack-merge');
 
-const common = require('./webpack.common.js');
-const CONFIG = require('../config.js');
+const common = require('./webpack/webpack.common.js');
+const css = require('./webpack/webpack.css.js');
 
-module.exports = merge(common, {
+const CONFIG = require('../config.js');
+const fileName = require('./fileName.js');
+
+module.exports = merge(common, css, {
   mode: 'development',
   dev_tool: 'inline-source-map',
 
   watch: true,
   output: {
-    filename: `${CONFIG.WEBPACK_BUNDLE_NAME}-test.js`
-  },
-  new SassPlugin(
-    {
-      "../scss/style.scss":"../css/style-dev.css"
-    },{
-    sourceMap: true,
-    sass: { outputStyle: 'compressed' },
-    autoprefixer: true
-  }, process.env.NODE_ENV)
-],
+    filename: fileName(CONFIG.BUNDLE.NAME, CONFIG.BUNDLE.EXT.DEV)
+  }
 });
