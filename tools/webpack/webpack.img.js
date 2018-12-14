146626b1c2311fa44c05e38d52286a07e651c7d6
Ralph van Barneveld <ralphvanbarneveld@gmail.com>
2018-12-14 23:10:51 +0100

    Add image-min to Webpack build (#11)

---
+++ tools/webpack/webpack.img.js
@@ -0,0 +1,19 @@
+var ImageminPlugin = require('imagemin-webpack-plugin').default
+var CopyWebpackPlugin = require('copy-webpack-plugin');
+var CONFIG = require('../../config.js');
+
+module.exports = {
+  plugins: [
+    new CopyWebpackPlugin([{
+      from: CONFIG.SRC.ROOT + CONFIG.SRC.IMG.PATH,
+      to: CONFIG.DIST.ROOT + CONFIG.DIST.IMG.PATH
+    }]),
+    // Make sure that the plugin is after any plugins that add images
+    new ImageminPlugin({
+      disable: process.env.NODE_ENV !== 'production', // Disable during development
+      pngquant: {
+        quality: '95-100'
+      }
+    }),
+  ]
+}
